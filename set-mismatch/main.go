afe47cdb3ca9e1bc5c01eff191ea0bb1ff0cf71f
dev-marees <mareescse104@gmail.com>
2026-02-13 13:40:25 +0530

    Refactor findErrorNums function and update main to include additional test cases

--- set-mismatch/main.go
+++ set-mismatch/main.go
@@ -1,15 +1,35 @@
 package main
 
+import "fmt"
+
 func main() {
 	nums := []int{1, 2, 2, 4}
+	nums2 := []int{1, 1}
+	nums3 := []int{2, 2}
+	fmt.Println("mis matched:", findErrorNums(nums))
+	fmt.Println("mis matched:", findErrorNums(nums2))
+	fmt.Println("mis matched:", findErrorNums(nums3))
 }
 
 func findErrorNums(nums []int) []int {
-	
-	for i := 0; i < len(nums); i++ {
-		 
 
+	temp := map[int]bool{}
+	result := []int{}
+
+	//actual sum
+	actualSum := 0
+	for i := 0; i < len(nums); i++ {
+		actualSum += nums[i]
 	}
-	
-	return nums
+
+	for i := 0; i < len(nums); i++ {
+		expectedSum := len(nums) * (len(nums) + 1) / 2
+		if _, ok := temp[nums[i]]; ok {
+			missedNumber := expectedSum - (actualSum - nums[i])
+			result = append(result, nums[i], missedNumber)
+		}
+		temp[nums[i]] = true
+	}
+
+	return result
 }
